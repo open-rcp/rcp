fdcff38c059225c5af0dc6201ac985cc0d2e447b
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-05-03 23:40:17 -0700

    refactor: Improve variable naming and update extension check in build.rs

--- examples/build.rs
+++ examples/build.rs
@@ -8,18 +8,22 @@ fn main() {
 
     // Get the output directory
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let target_dir = out_dir.ancestors().nth(3).unwrap(); // Go up to target/debug or target/release
+    let _target_dir = out_dir.ancestors().nth(3).unwrap(); // Go up to target/debug or target/release
 
     // No external libraries are currently being used
     println!("cargo:warning=No external libraries required");
+    
+    // Uncomment the following line if you need to copy DLL files
+    // let _ = copy_files(&_target_dir, &out_dir);
 }
 
+#[allow(dead_code)]
 fn copy_files(from_dir: &Path, to_dir: &Path) -> io::Result<()> {
     for entry in fs::read_dir(from_dir)? {
         let entry = entry?;
         let path = entry.path();
 
-        if path.is_file() && path.extension().map_or(false, |ext| ext == "dll") {
+        if path.is_file() && path.extension().is_some_and(|ext| ext == "dll") {
             let file_name = path.file_name().unwrap();
             let target_path = to_dir.join(file_name);
             println!(
