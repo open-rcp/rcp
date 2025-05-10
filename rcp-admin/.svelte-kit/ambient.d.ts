f14bb70c2e6f7397f7e6d407b789c9a37ad86ae5
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-05-10 16:18:04 +0530

    fix: remove duplicate environment variable declarations in ambient.d.ts

--- rcp-admin/.svelte-kit/ambient.d.ts
+++ rcp-admin/.svelte-kit/ambient.d.ts
@@ -51,6 +51,7 @@ declare module '$env/static/private' {
 	export const npm_execpath: string;
 	export const PATH: string;
 	export const npm_package_json: string;
+	export const _: string;
 	export const LaunchInstanceID: string;
 	export const npm_config_userconfig: string;
 	export const npm_config_init_module: string;
@@ -86,8 +87,6 @@ declare module '$env/static/private' {
 	export const npm_node_execpath: string;
 	export const npm_config_prefix: string;
 	export const COLORTERM: string;
-	export const _: string;
-	export const NODE_ENV: string;
 }
 
 /**
@@ -144,6 +143,7 @@ declare module '$env/dynamic/private' {
 		npm_execpath: string;
 		PATH: string;
 		npm_package_json: string;
+		_: string;
 		LaunchInstanceID: string;
 		npm_config_userconfig: string;
 		npm_config_init_module: string;
@@ -179,8 +179,6 @@ declare module '$env/dynamic/private' {
 		npm_node_execpath: string;
 		npm_config_prefix: string;
 		COLORTERM: string;
-		_: string;
-		NODE_ENV: string;
 		[key: `PUBLIC_${string}`]: undefined;
 		[key: `${string}`]: string | undefined;
 	}
